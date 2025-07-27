035b548f7e63af1aaae0f8e5af166222011ab187
DanielWinz75 <dwinz.inforecht@mailbox.org>
2025-07-27 15:29:05 +0200

    works for text/plain messages

--- public/background.js
+++ public/background.js
@@ -3,11 +3,11 @@ const aiReplyApiAddress = 'https://api.mistral.ai/v1/chat/completions';
 const aiReplyPromptMain = "General system instructions what to do: \nGenerate a reply to the email content provided below. Generate the reply text only. Don't generate any comments. Don't put the subject of the email into the reply message. Be concise with names for salutation and ending of the reply. The reply should be relevant to the email's subject and content. The reply should be in the same language as the original email. The reply should be concise and to the point. Do not include any personal information or sensitive data in the reply.";
 const aiReplyPromptFormat = "How to format the reply: \nThe reply should be formatted as a complete email text with a salutation and closing. The subject should not be repeated in the reply. The reply should be in plain text format. Do not include any HTML or rich text formatting in the reply.";
 const aiReplyModel = "mistral-small-latest";
-browser.storage.local.set({aiReplyApiKey});
-browser.storage.local.set({aiReplyApiAddress});
-browser.storage.local.set({aiReplyPromptMain});
-browser.storage.local.set({aiReplyPromptFormat});
-browser.storage.local.set({aiReplyModel});
+messenger.storage.local.set({aiReplyApiKey});
+messenger.storage.local.set({aiReplyApiAddress});
+messenger.storage.local.set({aiReplyPromptMain});
+messenger.storage.local.set({aiReplyPromptFormat});
+messenger.storage.local.set({aiReplyModel});
 
 const email = {
   messageId: "",
@@ -17,22 +17,23 @@ const email = {
   originalMessage: null,
   fullMessage: null,
   rawMessage: null,
-  errorOnFetchingEmailMessage: ""
+  errorOnFetchingEmailMessage: "",
+  isHtml: false
 };
 
-browser.runtime.onInstalled.addListener(() => {
+messenger.runtime.onInstalled.addListener(() => {
   console.log("Extension installed!");
 });
 
-browser.menus.create({
+messenger.menus.create({
   id: "ai-reply-with-the-chat",
   title: "AI Reply with the Chat",
   contexts: ["message_list"]
 });
 
-browser.menus.onClicked.addListener(async (info, tab) => {
+messenger.menus.onClicked.addListener(async (info, tab) => {
   if (info.menuItemId === "ai-reply-with-the-chat") {
-    win = await browser.windows.create({
+    win = await messenger.windows.create({
       url: "index.html",
       type: "popup",
       width: 1000,
@@ -47,9 +48,10 @@ browser.menus.onClicked.addListener(async (info, tab) => {
       email.subject = info.selectedMessages.messages[0].subject || "No Subject";
       email.from = info.selectedMessages.messages[0].author || "Unknown Sender";
 
-      browser.messages.getFull(email.messageId).then((message) => {
+      messenger.messages.getFull(email.messageId).then((message) => {
         console.log("Message content fetched:", message);         
         email.originalMessage = message;
+
       }).catch((error) => {
         console.error("Error fetching message content:", error);
         email.errorOnFetchingEmailMessage = error.message || "Unknown error";
@@ -69,8 +71,8 @@ async function fetchEmailContentFullAndRaw() {
 
   try {
     // getFull returns an EmailPart object
-    email.fullMessage = await browser.messages.getFull(email.messageId);
-
+    email.fullMessage = await messenger.messages.getFull(email.messageId);
+    checkPartsForContentType(email.fullMessage.parts);
     console.log("Full message fetched successfully:", email.fullMessage);
   } catch (err) {
     error = "Error on getFull message: "+err+"\n";
@@ -78,7 +80,7 @@ async function fetchEmailContentFullAndRaw() {
 
   try {
     // getRaw returns a string
-    email.rawMessage = await browser.messages.getRaw(email.messageId);
+    email.rawMessage = await messenger.messages.getRaw(email.messageId);
   } catch (err) {
     error += "Error on getRaw message: "+err;
   }
@@ -88,7 +90,7 @@ async function fetchEmailContentFullAndRaw() {
   }
 }
 
-browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
+messenger.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.action === "aiRG-getEmail") {
     sendResponse(email);
   }
@@ -107,9 +109,7 @@ browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
 
 async function fetchAIReply(prompt) {
 
-  // console.log("Fetching AI reply for prompt:", prompt);
-
-  const settings = await browser.storage.local.get({aiReplyApiKey, aiReplyApiAddress, aiReplyModel});
+  const settings = await messenger.storage.local.get({aiReplyApiKey, aiReplyApiAddress, aiReplyModel});
 
   const apiRequestBody = {
     model: settings.aiReplyModel,
@@ -139,31 +139,23 @@ async function fetchAIReply(prompt) {
 }
 
 async function closePopupAndOpenReplyWindow(aiGeneratedReply) {
-
-  let newBody = aiGeneratedReply + email.fullMessage.parts[0].body;
-
-  console.log("New body for reply:", newBody);
-
   try {
-    // let tab = await browser.compose.beginReply(email.messageId, "replyToAll", {isPlainText: true, plainTextBody: newBody, isModified: true});
+    tab = await messenger.compose.beginReply(email.messageId, "replyToAll", {isPlainText: !email.isHtml});
 
-    // console.log("window type: ", window.type, "tab id: ", tab.id);
+    let details = await messenger.compose.getComposeDetails(tab.id);
 
-    // let details = await browser.compose.getComposeDetails(tab.id);
-
-    let tab = await browser.compose.beginReply(email.messageId, "replyToAll", {
-      isPlainText: true,
-      plainTextBody: newBody
-    });
-
-    console.log("Neuer Compose-Tab ID:", tab.id);
-
-    let details = await browser.compose.getComposeDetails(tab.id);
-
-    console.log("Compose Details:", details);
+    console.log("Compose details fetched:", details);
+    if (details.isPlainText) {
+      await messenger.compose.setComposeDetails(tab.id, {
+        plainTextBody: aiGeneratedReply + "\n\n" + details.plainTextBody
+      });
+    } else {
+      await messenger.compose.setComposeDetails(tab.id, {
+        body: "<p>" + aiGeneratedReply + "</p><br><br>" + details.body
+      });
+    }    
 
     await browser.windows.remove(win.id);
-    console.log("Popup window closed successfully");
 
     return { success: true, tabId: tab.id };
   } catch (error) {
@@ -171,21 +163,49 @@ async function closePopupAndOpenReplyWindow(aiGeneratedReply) {
   }
 }
 
-  // let newBody = aiGeneratedReply + email.fullMessage.parts[0].body;
-  //let tab = await browser.compose.beginReply(email.messageId, "replyToAll"); //, {isPlainText: false, body: newBody, isModified: true});
-  // try {
-  //   let tab = await browser.compose.beginReply(email.messageId, "replyToAll"); //, {isPlainText: true, plainTextBody: newBody, isModified: true});
-  //   return { success: true, tabId: tab.id };
-  // } catch (error) {
-  //   return { success: false, error: error.message };
-  // }
 
-  // Body aktualisieren
-  // await browser.compose.setComposeDetails(tab.id, { body: newBody, isPlainText: true, isModified: true });
+function checkPartsForContentType(parts) {
+  for (let part of parts) {
+    if (part.contentType === "text/html") {
+      email.isHtml = true;
+    }
+    if (part.parts && part.parts.length > 0) {
+      checkPartsForContentType(part.parts);
+    }
+  }
+}
 
-  // Compose-Tab holen
-  // let details = await browser.compose.getComposeDetails(tab.id);
 
-  // Neuen Body bauen: Dein Text + bestehender Body (also das Zitat)
-  // let newBody = aiGeneratedReply + details.body;
 
+
+// Compose-Event-Handler
+// function handleComposeReady(info, state) {
+
+//     console.log("Compose state changed:", info, state, tab);
+
+//     if (info.tabId !== tab.id || info.state !== "ready") {
+//         return;
+//     }
+
+//     // Listener wieder entfernen, da wir ihn nur einmal brauchen
+//     messenger.compose.onComposeStateChanged.removeListener(handleComposeReady);
+
+//     (async () => {
+//         // Compose-Details holen
+//         let details = await messenger.compose.getComposeDetails(tab.id);
+
+//         // Body vorbereiten
+//         let newBody;
+//         if (details.isPlainText) {
+//             newBody = aiGeneratedReply + "\n\n" + details.body;
+//         } else {
+//             newBody = "<p>" + aiGeneratedReply + "</p><br><br>" + details.body;
+//         }
+
+//         console.log("Setting new body:", newBody);
+
+//         await messenger.compose.setComposeDetails(tab.id, { body: newBody });
+//     })();
+// }
+
+// messenger.compose.onComposeStateChanged.addListener(handleComposeReady);

93fc5055ca4854f6b6966b2c7962c22da1d12c59
DanielWinz75 <dwinz.inforecht@mailbox.org>
2025-07-28 14:58:26 +0200

    moved  secret from background.js to secret.json

--- public/background.js
+++ public/background.js
@@ -1,9 +1,9 @@
-const aiReplyApiKey = 'ClCEpJcm9Sh9h9r5R3plKSO8T6W1FHiJ';
+//let aiReplyApiKey = '';
 const aiReplyApiAddress = 'https://api.mistral.ai/v1/chat/completions';
 const aiReplyPromptMain = "General system instructions what to do: \nGenerate a reply to the email content provided below. Generate the reply text only. Don't generate any comments. Don't put the subject of the email into the reply message. Be concise with names for salutation and ending of the reply. The reply should be relevant to the email's subject and content. The reply should be in the same language as the original email. The reply should be concise and to the point. Do not include any personal information or sensitive data in the reply.";
 const aiReplyPromptFormat = "How to format the reply: \nThe reply should be formatted as a complete email text with a salutation and closing. The subject should not be repeated in the reply. The reply should be in plain text format. Do not include any HTML or rich text formatting in the reply.";
 const aiReplyModel = "mistral-small-latest";
-messenger.storage.local.set({aiReplyApiKey});
+//messenger.storage.local.set({aiReplyApiKey});
 messenger.storage.local.set({aiReplyApiAddress});
 messenger.storage.local.set({aiReplyPromptMain});
 messenger.storage.local.set({aiReplyPromptFormat});
@@ -108,8 +108,15 @@ messenger.runtime.onMessage.addListener((message, sender, sendResponse) => {
 });
 
 async function fetchAIReply(prompt) {
+  const fileresponse = await fetch('secret.json');
+  if (!fileresponse.ok) {
+    throw new Error(`HTTP-Fehler: ${fileresponse.status}`);
+  }
+  const data = await fileresponse.json();
 
-  const settings = await messenger.storage.local.get({aiReplyApiKey, aiReplyApiAddress, aiReplyModel});
+  const aiReplyApiKey = data.mistralsecret;
+
+  const settings = await messenger.storage.local.get({aiReplyApiAddress, aiReplyModel});
 
   const apiRequestBody = {
     model: settings.aiReplyModel,
@@ -121,7 +128,7 @@ async function fetchAIReply(prompt) {
   const response = await fetch(settings.aiReplyApiAddress, {
     method: "POST",
     headers: {
-      "Authorization": `Bearer ${settings.aiReplyApiKey}`,
+      "Authorization": `Bearer ${aiReplyApiKey}`,
       "Content-Type": "application/json",
       "Accept": "application/json"
     },
